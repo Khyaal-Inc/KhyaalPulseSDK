1fb4c59aff995e46dc22bfc6f92ee4d8e3a75723
Reefaq Kader <reefaq.mohammed@gmail.com>
2025-10-20 13:34:07 +0530

    add package file

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "YourFrameworkPackage",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
+    products: [
+        // This exposes your framework as a library to be imported by users.
+        .library(
+            name: "Pulse_iOS",
+            targets: ["Pulse_iOS"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Pulse_iOS",
+            url: "https://github.com/Khyaal-Inc/KhyaalPulseSDK/releases/download/0.0.1/Pulse_iOS.xcframework.zip",
+            checksum: "e271f7a1376bc9806d5dd2b272b8adc673afb293adb2ce8230cd813d9f886650"
+        )
+    ]
+)
