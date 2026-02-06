cfd18b001791d039f23c5e5006a41ec91635f04b
Reefaq Kader <reefaq.kader@gmail.com>
2026-02-06 16:24:33 +0530

    Update Pulse_iOS binary target to version 1.0.10

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Pulse_iOS",
-            url: "https://github.com/Khyaal-Inc/KhyaalPulseSDK/releases/download/sdks/Pulse-iOS_iOS_v1.0.9.xcframework.zip",
-            checksum: "edb6903dfde8b31f5f1e4242a1003b782dcf988777727b1257ef39ffcc24e0a1"
+            url: "https://github.com/Khyaal-Inc/KhyaalPulseSDK/releases/download/sdks/Pulse_iOS_v1.0.10.xcframework.zip",
+            checksum: "a13bb2a0f6a697c701cbdab45aad9edd509e55cfd267f53666c328ae4c887c53"
         )
     ]
 )
